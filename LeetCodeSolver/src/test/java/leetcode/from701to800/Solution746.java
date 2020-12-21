71296483af6112c929f0e776ef45e36bd47da93f
Zhu XH <zhuxiaohe95@163.com>
2020-12-21 11:27:42 +0800

    完成 力扣746题 力扣每日一题 746.使用最小花费爬楼梯

---
+++ LeetCodeSolver/src/test/java/leetcode/from701to800/Solution746.java
@@ -0,0 +1,52 @@
+package leetcode.from701to800;
+
+/**
+ * @Author: zhuxiaohe
+ * @Time: 2020/12/21 11:04
+ * @Description: 746. 使用最小花费爬楼梯 | 难度：简单 | 标签：数组、动态规划
+ * 数组的每个索引作为一个阶梯，第 i个阶梯对应着一个非负数的体力花费值 cost[i](索引从0开始)。
+ * <p>
+ * 每当你爬上一个阶梯你都要花费对应的体力花费值，然后你可以选择继续爬一个阶梯或者爬两个阶梯。
+ * <p>
+ * 您需要找到达到楼层顶部的最低花费。在开始时，你可以选择从索引为 0 或 1 的元素作为初始阶梯。
+ * <p>
+ * 示例 1:
+ * 输入: cost = [10, 15, 20]
+ * 输出: 15
+ * 解释: 最低花费是从cost[1]开始，然后走两步即可到阶梯顶，一共花费15。
+ * <p>
+ *  示例 2:
+ * 输入: cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
+ * 输出: 6
+ * 解释: 最低花费方式是从cost[0]开始，逐个经过那些1，跳过cost[3]，一共花费6。
+ * <p>
+ * 注意：
+ * cost 的长度将会在 [2, 1000]。
+ * 每一个 cost[i] 将会是一个Integer类型，范围为 [0, 999]。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/min-cost-climbing-stairs
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Modified By: zhuxiaohe
+ */
+public class Solution746 {
+    /**
+     * 执行用时： 1 ms , 在所有 Java 提交中击败了 99.68% 的用户
+     * 内存消耗： 38.3 MB , 在所有 Java 提交中击败了 49.32% 的用户
+     *
+     * @param cost
+     * @return
+     */
+    public int minCostClimbingStairs(int[] cost) {
+        if (cost.length == 2) {
+            return Math.min(cost[0], cost[1]);
+        }
+        int[] dp = new int[cost.length];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for (int i = 2; i < cost.length; i++) {
+            dp[i] = Math.min(dp[i - 1], dp[i - 2]) + cost[i];
+        }
+        return Math.min(dp[cost.length - 1], dp[cost.length - 2]);
+    }
+}
