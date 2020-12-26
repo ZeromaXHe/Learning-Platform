757ec5870d6b22762317beebe3384aa047a79b59
Zhu XH <zhuxiaohe95@163.com>
2020-12-26 16:24:14 +0800

    完成 力扣85题 力扣每日一题 85.最大矩形

---
+++ LeetCodeSolver/src/test/java/leetcode/first100/Solution85.java
@@ -0,0 +1,119 @@
+package leetcode.first100;
+
+import java.util.Stack;
+
+/**
+ * @Author: ZeromaXHe
+ * @Time: 2020/12/26 16:15
+ * @Description: 85.最大矩形 | 难度：困难 | 标签：栈、数组、哈希表、动态规划
+ * 给定一个仅包含 0 和 1 、大小为 rows x cols 的二维二进制矩阵，找出只包含 1 的最大矩形，并返回其面积。
+ * <p>
+ * 示例 1：
+ * 输入：matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+ * 输出：6
+ * 解释：最大矩形如上图所示。
+ * <p>
+ * 示例 2：
+ * 输入：matrix = []
+ * 输出：0
+ * <p>
+ * 示例 3：
+ * 输入：matrix = [["0"]]
+ * 输出：0
+ * <p>
+ * 示例 4：
+ * 输入：matrix = [["1"]]
+ * 输出：1
+ * <p>
+ * 示例 5：
+ * 输入：matrix = [["0","0"]]
+ * 输出：0
+ *  
+ * 提示：
+ * rows == matrix.length
+ * cols == matrix[0].length
+ * 0 <= row, cols <= 200
+ * matrix[i][j] 为 '0' 或 '1'
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/maximal-rectangle
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Modified By: ZeromaXHe
+ */
+public class Solution85 {
+    /**
+     * 执行用时： 9 ms , 在所有 Java 提交中击败了 55.81% 的用户
+     * 内存消耗： 41.4 MB , 在所有 Java 提交中击败了 74.30% 的用户
+     * 网友的题解，参考了84题，封装成了方法。
+     *
+     * @param matrix
+     * @return
+     */
+    public int maximalRectangle(char[][] matrix) {
+        if (matrix.length == 0) {
+            return 0;
+        }
+        int[] heights = new int[matrix[0].length];
+        int maxArea = 0;
+        for (int row = 0; row < matrix.length; row++) {
+            //遍历每一列，更新高度
+            for (int col = 0; col < matrix[0].length; col++) {
+                if (matrix[row][col] == '1') {
+                    heights[col] += 1;
+                } else {
+                    heights[col] = 0;
+                }
+            }
+            //调用上一题的解法，更新函数
+            maxArea = Math.max(maxArea, largestRectangleArea(heights));
+        }
+        return maxArea;
+    }
+
+    /**
+     * 84题封装的方法
+     *
+     * @param heights
+     * @return
+     */
+    public int largestRectangleArea(int[] heights) {
+        int maxArea = 0;
+        Stack<Integer> stack = new Stack<>();
+        int p = 0;
+        while (p < heights.length) {
+            //栈空入栈
+            if (stack.isEmpty()) {
+                stack.push(p);
+                p++;
+            } else {
+                int top = stack.peek();
+                //当前高度大于栈顶，入栈
+                if (heights[p] >= heights[top]) {
+                    stack.push(p);
+                    p++;
+                } else {
+                    //保存栈顶高度
+                    int height = heights[stack.pop()];
+                    //左边第一个小于当前柱子的下标
+                    int leftLessMin = stack.isEmpty() ? -1 : stack.peek();
+                    //右边第一个小于当前柱子的下标
+                    int RightLessMin = p;
+                    //计算面积
+                    int area = (RightLessMin - leftLessMin - 1) * height;
+                    maxArea = Math.max(area, maxArea);
+                }
+            }
+        }
+        while (!stack.isEmpty()) {
+            //保存栈顶高度
+            int height = heights[stack.pop()];
+            //左边第一个小于当前柱子的下标
+            int leftLessMin = stack.isEmpty() ? -1 : stack.peek();
+            //右边没有小于当前高度的柱子，所以赋值为数组的长度便于计算
+            int RightLessMin = heights.length;
+            int area = (RightLessMin - leftLessMin - 1) * height;
+            maxArea = Math.max(area, maxArea);
+        }
+        return maxArea;
+    }
+}
