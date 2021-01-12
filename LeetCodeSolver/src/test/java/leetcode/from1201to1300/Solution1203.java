7cc091f5e8ae5c5056a15798b7e84ccf3f50a2c9
Zhu XH <zhuxiaohe95@163.com>
2021-01-12 21:06:41 +0800

    未完成 力扣1203题 力扣每日一题 1203.项目管理

---
+++ LeetCodeSolver/src/test/java/leetcode/from1201to1300/Solution1203.java
@@ -0,0 +1,152 @@
+package leetcode.from1201to1300;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+
+/**
+ * @Author: ZeromaXHe
+ * @Time: 2021/1/12 20:37
+ * @Description: 1203.项目管理 | 难度：困难 | 标签：深度优先搜索、图、拓扑排序
+ * 公司共有 n 个项目和  m 个小组，每个项目要不无人接手，要不就由 m 个小组之一负责。
+ * <p>
+ * group[i] 表示第 i 个项目所属的小组，如果这个项目目前无人接手，那么 group[i] 就等于 -1。（项目和小组都是从零开始编号的）小组可能存在没有接手任何项目的情况。
+ * <p>
+ * 请你帮忙按要求安排这些项目的进度，并返回排序后的项目列表：
+ * <p>
+ * 同一小组的项目，排序后在列表中彼此相邻。
+ * 项目之间存在一定的依赖关系，我们用一个列表 beforeItems 来表示，其中 beforeItems[i] 表示在进行第 i 个项目前（位于第 i 个项目左侧）应该完成的所有项目。
+ * 如果存在多个解决方案，只需要返回其中任意一个即可。如果没有合适的解决方案，就请返回一个 空列表 。
+ * <p>
+ * 示例 1：
+ * 输入：n = 8, m = 2, group = [-1,-1,1,0,0,1,0,-1], beforeItems = [[],[6],[5],[6],[3,6],[],[],[]]
+ * 输出：[6,3,4,1,5,2,0,7]
+ * <p>
+ * 示例 2：
+ * 输入：n = 8, m = 2, group = [-1,-1,1,0,0,1,0,-1], beforeItems = [[],[6],[5],[6],[3],[],[4],[]]
+ * 输出：[]
+ * 解释：与示例 1 大致相同，但是在排序后的列表中，4 必须放在 6 的前面。
+ *  
+ * 提示：
+ * 1 <= m <= n <= 3 * 104
+ * group.length == beforeItems.length == n
+ * -1 <= group[i] <= m - 1
+ * 0 <= beforeItems[i].length <= n - 1
+ * 0 <= beforeItems[i][j] <= n - 1
+ * i != beforeItems[i][j]
+ * beforeItems[i] 不含重复元素
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/sort-items-by-groups-respecting-dependencies
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Modified By: ZeromaXHe
+ */
+public class Solution1203 {
+    /**
+     * 执行用时： 40 ms , 在所有 Java 提交中击败了 54.79% 的用户
+     * 内存消耗： 47.9 MB , 在所有 Java 提交中击败了 94.20% 的用户
+     * 官方题解
+     *
+     * @param n
+     * @param m
+     * @param group
+     * @param beforeItems
+     * @return
+     */
+    public int[] sortItems(int n, int m, int[] group, List<List<Integer>> beforeItems) {
+        // 第 1 步：数据预处理，给没有归属于一个组的项目编上组号
+        for (int i = 0; i < group.length; i++) {
+            if (group[i] == -1) {
+                group[i] = m;
+                m++;
+            }
+        }
+
+        // 第 2 步：实例化组和项目的邻接表
+        List<Integer>[] groupAdj = new ArrayList[m];
+        List<Integer>[] itemAdj = new ArrayList[n];
+        for (int i = 0; i < m; i++) {
+            groupAdj[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < n; i++) {
+            itemAdj[i] = new ArrayList<>();
+        }
+
+        // 第 3 步：建图和统计入度数组
+        int[] groupsIndegree = new int[m];
+        int[] itemsIndegree = new int[n];
+
+        int len = group.length;
+        for (int i = 0; i < len; i++) {
+            int currentGroup = group[i];
+            for (int beforeItem : beforeItems.get(i)) {
+                int beforeGroup = group[beforeItem];
+                if (beforeGroup != currentGroup) {
+                    groupAdj[beforeGroup].add(currentGroup);
+                    groupsIndegree[currentGroup]++;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (Integer item : beforeItems.get(i)) {
+                itemAdj[item].add(i);
+                itemsIndegree[i]++;
+            }
+        }
+
+        // 第 4 步：得到组和项目的拓扑排序结果
+        List<Integer> groupsList = topologicalSort(groupAdj, groupsIndegree, m);
+        if (groupsList.size() == 0) {
+            return new int[0];
+        }
+        List<Integer> itemsList = topologicalSort(itemAdj, itemsIndegree, n);
+        if (itemsList.size() == 0) {
+            return new int[0];
+        }
+
+        // 第 5 步：根据项目的拓扑排序结果，项目到组的多对一关系，建立组到项目的一对多关系
+        // key：组，value：在同一组的项目列表
+        Map<Integer, List<Integer>> groups2Items = new HashMap<>();
+        for (Integer item : itemsList) {
+            groups2Items.computeIfAbsent(group[item], key -> new ArrayList<>()).add(item);
+        }
+
+        // 第 6 步：把组的拓扑排序结果替换成为项目的拓扑排序结果
+        List<Integer> res = new ArrayList<>();
+        for (Integer groupId : groupsList) {
+            List<Integer> items = groups2Items.getOrDefault(groupId, new ArrayList<>());
+            res.addAll(items);
+        }
+        return res.stream().mapToInt(Integer::valueOf).toArray();
+    }
+
+    private List<Integer> topologicalSort(List<Integer>[] adj, int[] inDegree, int n) {
+        List<Integer> res = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            if (inDegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            Integer front = queue.poll();
+            res.add(front);
+            for (int successor : adj[front]) {
+                inDegree[successor]--;
+                if (inDegree[successor] == 0) {
+                    queue.offer(successor);
+                }
+            }
+        }
+
+        if (res.size() == n) {
+            return res;
+        }
+        return new ArrayList<>();
+    }
+}
