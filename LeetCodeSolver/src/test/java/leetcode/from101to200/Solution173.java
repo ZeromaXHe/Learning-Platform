8fcdfb1d298d5db39bd8d67c3ab344277f5dd463
Zhu XH <zhuxiaohe95@163.com>
2021-03-28 17:26:27 +0800

    完成 力扣第173题 （Java） 力扣每日一题 173.二叉搜索树迭代器

---
+++ LeetCodeSolver/src/test/java/leetcode/from101to200/Solution173.java
@@ -0,0 +1,121 @@
+package leetcode.from101to200;
+
+import java.util.LinkedList;
+
+/**
+ * @Author: ZeromaXHe
+ * @Time: 2021/3/28 17:20
+ * @Description: 173.二叉搜索树迭代器 | 难度：中等 | 标签：栈、树、设计
+ * 实现一个二叉搜索树迭代器类BSTIterator ，表示一个按中序遍历二叉搜索树（BST）的迭代器：
+ * BSTIterator(TreeNode root) 初始化 BSTIterator 类的一个对象。BST 的根节点 root 会作为构造函数的一部分给出。指针应初始化为一个不存在于 BST 中的数字，且该数字小于 BST 中的任何元素。
+ * boolean hasNext() 如果向指针右侧遍历存在数字，则返回 true ；否则返回 false 。
+ * int next()将指针向右移动，然后返回指针处的数字。
+ * 注意，指针初始化为一个不存在于 BST 中的数字，所以对 next() 的首次调用将返回 BST 中的最小元素。
+ * <p>
+ * 你可以假设 next() 调用总是有效的，也就是说，当调用 next() 时，BST 的中序遍历中至少存在一个下一个数字。
+ * <p>
+ * 示例：
+ * <p>
+ * 输入
+ * ["BSTIterator", "next", "next", "hasNext", "next", "hasNext", "next", "hasNext", "next", "hasNext"]
+ * [[[7, 3, 15, null, null, 9, 20]], [], [], [], [], [], [], [], [], []]
+ * 输出
+ * [null, 3, 7, true, 9, true, 15, true, 20, false]
+ * <p>
+ * 解释
+ * BSTIterator bSTIterator = new BSTIterator([7, 3, 15, null, null, 9, 20]);
+ * bSTIterator.next();    // 返回 3
+ * bSTIterator.next();    // 返回 7
+ * bSTIterator.hasNext(); // 返回 True
+ * bSTIterator.next();    // 返回 9
+ * bSTIterator.hasNext(); // 返回 True
+ * bSTIterator.next();    // 返回 15
+ * bSTIterator.hasNext(); // 返回 True
+ * bSTIterator.next();    // 返回 20
+ * bSTIterator.hasNext(); // 返回 False
+ * <p>
+ * 提示：
+ * 树中节点的数目在范围 [1, 105] 内
+ * 0 <= Node.val <= 106
+ * 最多调用 105 次 hasNext 和 next 操作
+ * <p>
+ * 进阶：
+ * 你可以设计一个满足下述条件的解决方案吗？next() 和 hasNext() 操作均摊时间复杂度为 O(1) ，并使用 O(h) 内存。其中 h 是树的高度。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/binary-search-tree-iterator
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Modified By: ZeromaXHe
+ */
+public class Solution173 {
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     * <p>
+     * 执行用时： 22 ms , 在所有 Java 提交中击败了 95.60% 的用户
+     * 内存消耗： 41.9 MB , 在所有 Java 提交中击败了 71.45% 的用户
+     * <p>
+     * 题解答案
+     */
+    class BSTIterator {
+        private TreeNode cur;
+        private LinkedList<TreeNode> stack;
+
+        public BSTIterator(TreeNode root) {
+            cur = root;
+            stack = new LinkedList<>();
+        }
+
+        public int next() {
+            while (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            int ret = cur.val;
+            cur = cur.right;
+            return ret;
+        }
+
+        public boolean hasNext() {
+            return cur != null || !stack.isEmpty();
+        }
+    }
+
+    /**
+     * Your BSTIterator object will be instantiated and called as such:
+     * BSTIterator obj = new BSTIterator(root);
+     * int param_1 = obj.next();
+     * boolean param_2 = obj.hasNext();
+     */
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
