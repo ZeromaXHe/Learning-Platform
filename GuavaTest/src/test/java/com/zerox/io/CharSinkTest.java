e52a7e9d3fb868455b0703cc470967b9b0355f55
Zhu XH <zhuxiaohe95@163.com>
2021-07-03 20:19:57 +0800

    更新 GuavaTest 汪文君Google Guava第11讲 CharSource、CharSink

---
+++ GuavaTest/src/test/java/com/zerox/io/CharSinkTest.java
@@ -0,0 +1,21 @@
+package com.zerox.io;
+
+import com.google.common.io.Files;
+import org.junit.Test;
+
+/**
+ * @Author: ZeromaXHe
+ * @Time: 2021/7/3 20:09
+ * @Description:
+ * @Modified By: ZeromaXHe
+ */
+public class CharSinkTest {
+    /**
+     * CharSource --> Reader
+     * CharSink   --> Writer
+     */
+    @Test
+    public void testCharSink() {
+//        Files.asCharSink();
+    }
+}
---
+++ GuavaTest/src/test/java/com/zerox/io/CharSourceTest.java
@@ -0,0 +1,43 @@
+package com.zerox.io;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.io.CharSource;
+import com.google.common.io.Files;
+import org.hamcrest.core.IsEqual;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+
+/**
+ * @Author: ZeromaXHe
+ * @Time: 2021/7/3 19:57
+ * @Description:
+ * @Modified By: ZeromaXHe
+ */
+public class CharSourceTest {
+    @Test
+    public void testCharSourceWrap() throws IOException {
+        CharSource charSource = CharSource.wrap("i am the CharSource");
+        String resultAsRead = charSource.read();
+        Assert.assertThat(resultAsRead, IsEqual.equalTo("i am the CharSource"));
+        ImmutableList<String> list = charSource.readLines();
+        Assert.assertThat(list.size(), IsEqual.equalTo(1));
+        Assert.assertThat(charSource.length(), IsEqual.equalTo(19L));
+        Assert.assertThat(charSource.isEmpty(), IsEqual.equalTo(false));
+        Assert.assertThat(charSource.lengthIfKnown().get(), IsEqual.equalTo(19L));
+
+//        Files.asCharSource();
+    }
+
+    @Test
+    public void testConcat() throws IOException {
+        CharSource charSource = CharSource.concat(
+                CharSource.wrap("i am the CharSource1\n"),
+                CharSource.wrap("i am the CharSource2")
+        );
+
+        Assert.assertThat(charSource.readLines().size(), IsEqual.equalTo(2));
+        charSource.lines().forEach(System.out::println);
+    }
+}
