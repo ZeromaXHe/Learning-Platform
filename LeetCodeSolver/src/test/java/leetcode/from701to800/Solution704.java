ff5521b454f28dfd05d2ac55b2241ba7d8e2ebe9
Zhu XH <zhuxiaohe95@163.com>
2021-09-06 10:15:05 +0800

    完成 力扣第704题 （Java） 力扣每日一题 704.二分查找

---
+++ LeetCodeSolver/src/test/java/leetcode/from701to800/Solution704.java
@@ -0,0 +1,60 @@
+package leetcode.from701to800;
+
+/**
+ * @Author: zhuxi
+ * @Time: 2021/9/6 9:57
+ * @Description: 704. 二分查找 | 难度：简单 | 标签：数组、二分查找
+ * 给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
+ * <p>
+ * 示例 1:
+ * 输入: nums = [-1,0,3,5,9,12], target = 9
+ * 输出: 4
+ * 解释: 9 出现在 nums 中并且下标为 4
+ * <p>
+ * 示例 2:
+ * 输入: nums = [-1,0,3,5,9,12], target = 2
+ * 输出: -1
+ * 解释: 2 不存在 nums 中因此返回 -1
+ * <p>
+ * 提示：
+ * 你可以假设 nums 中的所有元素是不重复的。
+ * n 将在 [1, 10000]之间。
+ * nums 的每个元素都将在 [-9999, 9999]之间。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/binary-search
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @ModifiedBy: zhuxi
+ */
+public class Solution704 {
+    /**
+     * 执行用时： 0 ms , 在所有 Java 提交中击败了 100.00% 的用户
+     * 内存消耗： 39 MB , 在所有 Java 提交中击败了 96.26% 的用户
+     * 通过测试用例： 46 / 46
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int mid = (left + right) / 2;
+        while (right > left) {
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+                mid = (left + right) / 2;
+            } else {
+                left = mid + 1;
+                mid = (left + right) / 2;
+            }
+        }
+        if (nums[mid] == target) {
+            return mid;
+        } else {
+            return -1;
+        }
+    }
+}
