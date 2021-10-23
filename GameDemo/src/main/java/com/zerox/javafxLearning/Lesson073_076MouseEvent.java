9d4465ecf8a070e7bbd8ad93294586c82605cf55
Zhu XH <zhuxiaohe95@163.com>
2021-10-23 18:15:24 +0800

    更新 GameDemo javafxLearning 参考 B站 Aimls JavaFX视频教程 第75~77课

--- GameDemo/src/main/java/com/zerox/javafxLearning/Lesson073_076MouseEvent.java
+++ GameDemo/src/main/java/com/zerox/javafxLearning/Lesson073_076MouseEvent.java
@@ -1,10 +1,14 @@
 package com.zerox.javafxLearning;
 
 import javafx.application.Application;
+import javafx.event.Event;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 /**
@@ -13,7 +17,7 @@ import javafx.stage.Stage;
  * @Description:
  * @ModifiedBy: ZeromaXHe
  */
-public class Lesson073_074MouseEvent extends Application {
+public class Lesson073_076MouseEvent extends Application {
     public static void main(String[] args) {
         launch(args);
     }
@@ -21,11 +25,18 @@ public class Lesson073_074MouseEvent extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         HBox hBox = new HBox(20);
+        hBox.setStyle("-fx-background-color: #ffff55");
 
         Button b1 = new Button("button1");
         Button b2 = new Button("button2");
 
-        hBox.getChildren().addAll(b1, b2);
+        Circle cir = new Circle(100, Color.RED);
+        // 整个矩形范围有效
+        cir.setPickOnBounds(true);
+        // 阻止事件从父组件传递给子组件，true就只有hBox的了，false的话还有cir的
+        cir.setMouseTransparent(true);
+
+        hBox.getChildren().addAll(b1, b2, cir);
 
         b1.setOnMouseClicked(event -> {
             System.out.println("x = " + event.getX());
@@ -43,13 +54,22 @@ public class Lesson073_074MouseEvent extends Application {
             System.out.println("ctrlDown: " + event.isControlDown());
             System.out.println("clickCount: " + event.getClickCount());
             System.out.println("secondaryButtonDown: " + event.isSecondaryButtonDown());
+
+            // 是否触摸屏
+            System.out.println(event.isSynthesized());
+            MouseEvent me = event.copyFor(b2, b2);
+            Event.fireEvent(me.getTarget(), me);
         });
 
-        b1.setOnMousePressed(event -> System.out.println("鼠标按下"));
+        b1.setOnMousePressed(event -> {
+            System.out.println("鼠标按下");
+            System.out.println(event.isStillSincePress());
+        });
         b1.setOnMouseReleased(event -> System.out.println("鼠标释放"));
         b1.setOnMouseEntered(event -> System.out.println("鼠标进入"));
         b1.setOnMouseExited(event -> System.out.println("鼠标退出"));
 
+        b2.setOnMouseClicked(event -> System.out.println("点击B2"));
         b2.setOnMouseMoved(event -> System.out.println("鼠标移动"));
         b2.setOnMouseDragged(event -> System.out.println("鼠标拖拽"));
         b2.setOnMouseDragOver(event -> System.out.println("dragOver"));
@@ -66,6 +86,13 @@ public class Lesson073_074MouseEvent extends Application {
             System.out.println("source:" + event.getSource());
             System.out.println("gestureSource:" + event.getGestureSource());
             System.out.println("鼠标拖拽释放");
+        });
+
+        cir.setOnMouseClicked(event -> System.out.println("cir.setOnMouseClicked"));
+
+        hBox.setOnMouseClicked(event -> {
+            System.out.println("hBox.setOnMouseClicked");
+            System.out.println(event.getPickResult());
         });
 
         AnchorPane an = new AnchorPane();
