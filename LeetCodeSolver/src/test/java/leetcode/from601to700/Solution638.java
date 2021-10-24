1101c467364f90871c8b3ef4f18027b3b2c40f97
Zhu XH <zhuxiaohe95@163.com>
2021-10-24 09:03:16 +0800

    完成 力扣第638题（Java） 力扣每日一题 638. 大礼包

---
+++ LeetCodeSolver/src/test/java/leetcode/from601to700/Solution638.java
@@ -0,0 +1,109 @@
+package leetcode.from601to700;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Author: ZeromaXHe
+ * @Time: 2021/10/24 8:43
+ * @Description: 638. 大礼包 | 难度：中等 | 标签：位运算、记忆化搜索、数组、动态规划、回溯、状态压缩
+ * 在 LeetCode 商店中， 有 n 件在售的物品。每件物品都有对应的价格。然而，也有一些大礼包，每个大礼包以优惠的价格捆绑销售一组物品。
+ * <p>
+ * 给你一个整数数组 price 表示物品价格，其中 price[i] 是第 i 件物品的价格。另有一个整数数组 needs 表示购物清单，其中 needs[i] 是需要购买第 i 件物品的数量。
+ * <p>
+ * 还有一个数组 special 表示大礼包，special[i] 的长度为 n + 1 ，其中 special[i][j] 表示第 i 个大礼包中内含第 j 件物品的数量，且 special[i][n] （也就是数组中的最后一个整数）为第 i 个大礼包的价格。
+ * <p>
+ * 返回 确切 满足购物清单所需花费的最低价格，你可以充分利用大礼包的优惠活动。你不能购买超出购物清单指定数量的物品，即使那样会降低整体价格。任意大礼包可无限次购买。
+ * <p>
+ * 示例 1：
+ * 输入：price = [2,5], special = [[3,0,5],[1,2,10]], needs = [3,2]
+ * 输出：14
+ * 解释：有 A 和 B 两种物品，价格分别为 ¥2 和 ¥5 。
+ * 大礼包 1 ，你可以以 ¥5 的价格购买 3A 和 0B 。
+ * 大礼包 2 ，你可以以 ¥10 的价格购买 1A 和 2B 。
+ * 需要购买 3 个 A 和 2 个 B ， 所以付 ¥10 购买 1A 和 2B（大礼包 2），以及 ¥4 购买 2A 。
+ * <p>
+ * 示例 2：
+ * 输入：price = [2,3,4], special = [[1,1,0,4],[2,2,1,9]], needs = [1,2,1]
+ * 输出：11
+ * 解释：A ，B ，C 的价格分别为 ¥2 ，¥3 ，¥4 。
+ * 可以用 ¥4 购买 1A 和 1B ，也可以用 ¥9 购买 2A ，2B 和 1C 。
+ * 需要买 1A ，2B 和 1C ，所以付 ¥4 买 1A 和 1B（大礼包 1），以及 ¥3 购买 1B ， ¥4 购买 1C 。
+ * 不可以购买超出待购清单的物品，尽管购买大礼包 2 更加便宜。
+ * <p>
+ * 提示：
+ * n == price.length
+ * n == needs.length
+ * 1 <= n <= 6
+ * 0 <= price[i] <= 10
+ * 0 <= needs[i] <= 10
+ * 1 <= special.length <= 100
+ * special[i].length == n + 1
+ * 0 <= special[i][j] <= 50
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/shopping-offers
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @ModifiedBy: ZeromaXHe
+ */
+public class Solution638 {
+    private Map<List<Integer>, Integer> memo = new HashMap<>();
+
+    /**
+     * 执行用时： 7 ms , 在所有 Java 提交中击败了 57.14% 的用户
+     * 内存消耗： 37.6 MB , 在所有 Java 提交中击败了 96.29% 的用户
+     * 通过测试用例： 64 / 64
+     * <p>
+     * 官方题解
+     *
+     * @param price
+     * @param special
+     * @param needs
+     * @return
+     */
+    public int shoppingOffers(List<Integer> price, List<List<Integer>> special, List<Integer> needs) {
+        int n = price.size();
+
+        // 过滤不需要计算的大礼包，只保留需要计算的大礼包
+        List<List<Integer>> filterSpecial = new ArrayList<>();
+        for (List<Integer> sp : special) {
+            int totalCount = 0, totalPrice = 0;
+            for (int i = 0; i < n; ++i) {
+                totalCount += sp.get(i);
+                totalPrice += sp.get(i) * price.get(i);
+            }
+            if (totalCount > 0 && totalPrice > sp.get(n)) {
+                filterSpecial.add(sp);
+            }
+        }
+
+        return dfs(price, needs, filterSpecial, n);
+    }
+
+    // 记忆化搜索计算满足购物清单所需花费的最低价格
+    private int dfs(List<Integer> price, List<Integer> curNeeds, List<List<Integer>> filterSpecial, int n) {
+        if (!memo.containsKey(curNeeds)) {
+            int minPrice = 0;
+            for (int i = 0; i < n; ++i) {
+                minPrice += curNeeds.get(i) * price.get(i); // 不购买任何大礼包，原价购买购物清单中的所有物品
+            }
+            for (List<Integer> curSpecial : filterSpecial) {
+                int specialPrice = curSpecial.get(n);
+                List<Integer> nxtNeeds = new ArrayList<>();
+                for (int i = 0; i < n; ++i) {
+                    if (curSpecial.get(i) > curNeeds.get(i)) { // 不能购买超出购物清单指定数量的物品
+                        break;
+                    }
+                    nxtNeeds.add(curNeeds.get(i) - curSpecial.get(i));
+                }
+                if (nxtNeeds.size() == n) { // 大礼包可以购买
+                    minPrice = Math.min(minPrice, dfs(price, nxtNeeds, filterSpecial, n) + specialPrice);
+                }
+            }
+            memo.put(curNeeds, minPrice);
+        }
+        return memo.get(curNeeds);
+    }
+}
