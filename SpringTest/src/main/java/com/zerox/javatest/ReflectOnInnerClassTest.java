3572a8c49ad39f7be0a3e340a1460253fea16369
Zhu XH <zhuxiaohe95@163.com>
2022-03-30 17:31:48 +0800

    修改 springTest javatest 反射获取内部类测试：修改打印逻辑、添加局部内部类无法反射的案例

--- SpringTest/src/main/java/com/zerox/javatest/ReflectOnInnerClassTest.java
+++ SpringTest/src/main/java/com/zerox/javatest/ReflectOnInnerClassTest.java
@@ -2,7 +2,6 @@ package com.zerox.javatest;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 
@@ -14,30 +13,44 @@ import java.lang.reflect.Modifier;
  */
 public class ReflectOnInnerClassTest {
     static class InnerContainer {
-        public InnerContainer() {
-        }
+        public void localInnerTest() {
+            // 局部内部类
+            class LocalInner {
+                private String f = LocalInner.class.getSimpleName();
 
-        private class InnerA {
-            private String f = InnerA.class.getSimpleName();
-
-            public InnerA() {
+                public void test() {
+                    System.out.println(f);
+                }
             }
+
+            new LocalInner().test();
         }
 
-        private static class InnerB {
-            private String f = InnerB.class.getSimpleName();
+        // 普通内部类
+        class Inner {
+            private String f = Inner.class.getSimpleName();
+        }
 
-            public InnerB() {
+        // 静态内部类
+        static class StaticInner {
+            private String f = StaticInner.class.getSimpleName();
+        }
+
+        // 匿名内部类
+        Runnable anonymousInner = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Method run of Runnable anonymousInner");
             }
-        }
+        };
 
-        private Runnable r = () -> System.out.println("Method run of Runnable r");
+        Runnable lambda = () -> System.out.println("Method run of Runnable lambda");
     }
 
-    public static void main(String[] args) throws IllegalAccessException, InstantiationException, NoSuchFieldException,
-            NoSuchMethodException, InvocationTargetException, ClassNotFoundException {
-        printInner();
+    public static void main(String[] args) throws ClassNotFoundException {
         printAllReflections("com.zerox.javatest.ReflectOnInnerClassTest$InnerContainer");
+        printAllReflections("com.zerox.javatest.ReflectOnInnerClassTest$InnerContainer$Inner");
+        printAllReflections("com.zerox.javatest.ReflectOnInnerClassTest$InnerContainer$StaticInner");
     }
 
     public static void printAllReflections(String className) throws ClassNotFoundException {
@@ -53,107 +66,93 @@ public class ReflectOnInnerClassTest {
         }
         System.out.print("\n{\n");
         printConstructors(cl);
-        System.out.println();
         printMethods(cl);
-        System.out.println();
         printFields(cl);
-        System.out.println();
+        printInner(cl);
         System.out.println("}");
     }
 
     public static void printConstructors(Class cl) {
         Constructor[] constructors = cl.getDeclaredConstructors();
-
-        for (Constructor c : constructors) {
-            String name = c.getName();
-            System.out.print("   ");
-            String modifiers = Modifier.toString(c.getModifiers());
-            if (modifiers.length() > 0) {
-                System.out.print(modifiers + " ");
-            }
-            System.out.print(name + "(");
-
-            Class[] paramTypes = c.getParameterTypes();
-            for (int i = 0; i < paramTypes.length; i++) {
-                if (i > 0) {
-                    System.out.print(", ");
+        if (constructors.length > 0) {
+            System.out.println("    构造方法：");
+            for (Constructor c : constructors) {
+                String name = c.getName();
+                System.out.print("    ");
+                String modifiers = Modifier.toString(c.getModifiers());
+                if (modifiers.length() > 0) {
+                    System.out.print(modifiers + " ");
                 }
-                System.out.print(paramTypes[i].getName());
+                System.out.print(name + "(");
+
+                Class[] paramTypes = c.getParameterTypes();
+                for (int i = 0; i < paramTypes.length; i++) {
+                    if (i > 0) {
+                        System.out.print(", ");
+                    }
+                    System.out.print(paramTypes[i].getName());
+                }
+                System.out.println(");");
             }
-            System.out.println(");");
         }
     }
 
     public static void printMethods(Class cl) {
         Method[] methods = cl.getDeclaredMethods();
+        if (methods.length > 0) {
+            System.out.println("    方法：");
+            for (Method m : methods) {
+                Class retType = m.getReturnType();
+                String name = m.getName();
 
-        for (Method m : methods) {
-            Class retType = m.getReturnType();
-            String name = m.getName();
-
-            System.out.print("   ");
-            String modifiers = Modifier.toString(m.getModifiers());
-            if (modifiers.length() > 0) {
-                System.out.print(modifiers + " ");
-            }
-            System.out.print(retType.getName() + " " + name + "(");
-
-            Class[] paramTypes = m.getParameterTypes();
-            for (int i = 0; i < paramTypes.length; i++) {
-                if (i > 0) {
-                    System.out.print(", ");
+                System.out.print("    ");
+                String modifiers = Modifier.toString(m.getModifiers());
+                if (modifiers.length() > 0) {
+                    System.out.print(modifiers + " ");
                 }
-                System.out.print(paramTypes[i].getName());
+                System.out.print(retType.getName() + " " + name + "(");
+
+                Class[] paramTypes = m.getParameterTypes();
+                for (int i = 0; i < paramTypes.length; i++) {
+                    if (i > 0) {
+                        System.out.print(", ");
+                    }
+                    System.out.print(paramTypes[i].getName());
+                }
+                System.out.println(");");
             }
-            System.out.println(");");
         }
     }
 
     public static void printFields(Class cl) {
         Field[] fields = cl.getDeclaredFields();
-
-        for (Field f : fields) {
-            Class type = f.getType();
-            String name = f.getName();
-            System.out.print("   ");
-            String modifiers = Modifier.toString(f.getModifiers());
-            if (modifiers.length() > 0) {
-                System.out.print(modifiers + " ");
+        if (fields.length > 0) {
+            System.out.println("    成员变量：");
+            for (Field f : fields) {
+                Class type = f.getType();
+                String name = f.getName();
+                System.out.print("    ");
+                String modifiers = Modifier.toString(f.getModifiers());
+                if (modifiers.length() > 0) {
+                    System.out.print(modifiers + " ");
+                }
+                System.out.println(type.getName() + " " + name + ";");
             }
-            System.out.print(type.getName() + " " + name + ";");
         }
     }
 
-    private static void printInner() throws InstantiationException, IllegalAccessException, NoSuchFieldException,
-            NoSuchMethodException, InvocationTargetException {
-        Class clazz = InnerContainer.class;
-        InnerContainer container = (InnerContainer) clazz.newInstance();
-        Class[] innerClazz = clazz.getDeclaredClasses();
-        for (Class cls : innerClazz) {
-            System.out.println(cls.getName());
-            int mod = cls.getModifiers();
-            String modifier = Modifier.toString(mod);
-            if (modifier.contains("static")) {
-                //构造静态内部类实例
-//              Constructor con1 = cls.getDeclaredConstructor();
-                Object obj1 = cls.newInstance();
-                Field field1 = cls.getDeclaredField("f");
-                field1.setAccessible(true);
-                System.out.println(field1.get(obj1));
-            } else {
-                // 构造成员内部类实例
-                Constructor con2 = cls.getDeclaredConstructor(clazz);
-                con2.setAccessible(true);
-                Object obj2 = con2.newInstance(container);
-                Field field2 = cls.getDeclaredField("f");
-                field2.setAccessible(true);
-                System.out.println(field2.get(obj2));
+    private static void printInner(Class cl) {
+        Class[] innerClazz = cl.getDeclaredClasses();
+        if (innerClazz.length > 0) {
+            System.out.println("    内部类：");
+            for (Class cls : innerClazz) {
+                System.out.print("    ");
+                String modifiers = Modifier.toString(cls.getModifiers());
+                if (modifiers.length() > 0) {
+                    System.out.print(modifiers + " ");
+                }
+                System.out.println(cls.getName());
             }
         }
-        // 获取匿名内部类实例
-        Field field = clazz.getDeclaredField("r");
-        field.setAccessible(true);
-        Runnable r = (Runnable) field.get(container);
-        r.run();
     }
 }
