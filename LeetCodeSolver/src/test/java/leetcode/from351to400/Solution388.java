b07aee579fabe822da0e5794a100e5b50582fbec
Zhu XH <zhuxiaohe95@163.com>
2022-04-20 12:09:04 +0800

    完成 力扣第388题 （Java） 力扣每日一题 388. 文件的最长绝对路径

---
+++ LeetCodeSolver/src/test/java/leetcode/from351to400/Solution388.java
@@ -0,0 +1,187 @@
+package leetcode.from351to400;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.LinkedList;
+
+/**
+ * @Author: zhuxi
+ * @Time: 2022/4/20 10:03
+ * @Description: 388. 文件的最长绝对路径 | 难度：中等 | 标签：栈、深度优先搜索、字符串
+ * 假设有一个同时存储文件和目录的文件系统。下图展示了文件系统的一个示例：
+ * <p>
+ * 这里将 dir 作为根目录中的唯一目录。dir 包含两个子目录 subdir1 和 subdir2 。subdir1 包含文件 file1.ext 和子目录 subsubdir1；subdir2 包含子目录 subsubdir2，该子目录下包含文件 file2.ext 。
+ * <p>
+ * 在文本格式中，如下所示(⟶表示制表符)：
+ * <p>
+ * dir
+ * ⟶ subdir1
+ * ⟶ ⟶ file1.ext
+ * ⟶ ⟶ subsubdir1
+ * ⟶ subdir2
+ * ⟶ ⟶ subsubdir2
+ * ⟶ ⟶ ⟶ file2.ext
+ * 如果是代码表示，上面的文件系统可以写为 "dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext" 。'\n' 和 '\t' 分别是换行符和制表符。
+ * <p>
+ * 文件系统中的每个文件和文件夹都有一个唯一的 绝对路径 ，即必须打开才能到达文件/目录所在位置的目录顺序，所有路径用 '/' 连接。上面例子中，指向 file2.ext 的 绝对路径 是 "dir/subdir2/subsubdir2/file2.ext" 。每个目录名由字母、数字和/或空格组成，每个文件名遵循 name.extension 的格式，其中 name 和 extension由字母、数字和/或空格组成。
+ * <p>
+ * 给定一个以上述格式表示文件系统的字符串 input ，返回文件系统中 指向 文件 的 最长绝对路径 的长度 。 如果系统中没有文件，返回 0。
+ * <p>
+ * 示例 1：
+ * 输入：input = "dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext"
+ * 输出：20
+ * 解释：只有一个文件，绝对路径为 "dir/subdir2/file.ext" ，路径长度 20
+ * <p>
+ * 示例 2：
+ * 输入：input = "dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext"
+ * 输出：32
+ * 解释：存在两个文件：
+ * "dir/subdir1/file1.ext" ，路径长度 21
+ * "dir/subdir2/subsubdir2/file2.ext" ，路径长度 32
+ * 返回 32 ，因为这是最长的路径
+ * <p>
+ * 示例 3：
+ * 输入：input = "a"
+ * 输出：0
+ * 解释：不存在任何文件
+ * <p>
+ * 示例 4：
+ * 输入：input = "file1.txt\nfile2.txt\nlongfile.txt"
+ * 输出：12
+ * 解释：根目录下有 3 个文件。
+ * 因为根目录中任何东西的绝对路径只是名称本身，所以答案是 "longfile.txt" ，路径长度为 12
+ * <p>
+ * 提示：
+ * 1 <= input.length <= 104
+ * input 可能包含小写或大写的英文字母，一个换行符 '\n'，一个制表符 '\t'，一个点 '.'，一个空格 ' '，和数字。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/longest-absolute-file-path
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @ModifiedBy: zhuxi
+ */
+public class Solution388 {
+    @Test
+    public void lengthLongestPathTest() {
+        Assert.assertEquals(20, lengthLongestPath("dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext"));
+        Assert.assertEquals(32, lengthLongestPath("dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext"));
+        Assert.assertEquals(0, lengthLongestPath("a"));
+        Assert.assertEquals(13, lengthLongestPath("a\n\taa.aaa\n\taaa\n\t\taaa\n\t\t\tb.b"));
+        Assert.assertEquals(12, lengthLongestPath("file1.txt\nfile2.txt\nlongfile.txt"));
+        Assert.assertEquals(528, lengthLongestPath("sladjf\n\tlkjlkv\n\t\tlkjlakjlert\n\t\t\tlaskjglaksjf\n\t\t\t\tlakjgfljrtlj\n\t\t\t\t\tlskajflakjsvlj\n\t\t\t\t\t\tlskgjflkjrtlrjt\n\t\t\t\t\t\t\tlkjglkjlvkjdlvkj\n\t\t\t\t\t\t\t\tlfjkglkjfljdlv\n\t\t\t\t\t\t\t\t\tlkdfjglerjtkrjkljsd.lkvjlkajlfk\n\t\t\t\t\t\t\tlskfjlksjljslvjxjlvkzjljajoiwjejlskjslfj.slkjflskjldfkjoietruioskljfkljf\n\t\t\t\t\tlkasjfljsaljlxkcjzljvl.asljlksaj\n\t\t\t\tasldjflksajf\n\t\t\t\talskjflkasjlvkja\n\t\t\t\twioeuoiwutrljsgfjlskfg\n\t\t\t\taslkjvlksjvlkjsflgj\n\t\t\t\t\tlkvnlksfgk.salfkjaslfjskljfv\n\t\t\tlksdjflsajlkfj\n\t\t\tlasjflaskjlk\n\t\tlsakjflkasjfkljas\n\t\tlskjvljvlkjlsjfkgljfg\n\tsaljkglksajvlkjvkljlkjvksdj\n\tlsakjglksajkvjlkjdklvj\n\tlskjflksjglkdjbkljdbkjslkj\n\t\tlkjglkfjkljsdflj\n\t\t\tlskjfglkjdfgkljsdflj\n\t\t\t\tlkfjglksdjlkjbsdlkjbk\n\t\t\t\t\tlkfgjlejrtljkljsdflgjl\n\t\t\t\t\tsalgkfjlksfjgkljsgfjl\n\t\t\t\t\tsalkflajwoieu\n\t\t\t\t\t\tlaskjfglsjfgljkkvjsdlkjbklds\n\t\t\t\t\t\t\tlasjglriotuojgkjsldfgjsklfgjl\n\t\t\t\t\t\t\t\tlkajglkjskljsdljblkdfjblfjlbjs\n\t\t\t\t\t\t\t\t\tlkajgljroituksfglkjslkjgoi\n\t\t\t\t\t\t\t\t\t\tlkjglkjkljkljdkbljsdfljgklfdj\n\t\t\t\t\t\t\t\t\t\t\tlkjlgkjljgslkdkldjblkj\n\t\t\t\t\t\t\t\t\t\t\t\tlkjfglkjlkjbsdklj.slgfjalksjglkfjglf\n\t\t\t\t\t\t\t\t\t\t\t\tlkasjrlkjwlrjljsl\n\t\t\t\t\t\t\t\t\t\t\t\t\tlksjgflkjfklgjljbljls\n\t\t\t\t\t\t\t\t\t\t\t\t\t\tlkjsglkjlkjfkljdklbjkldf\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tlkjglkjdlsfjdglsdjgjlxljjlrjsgjsjlk\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tlkjsgkllksjfgjljdslfkjlkasjdflkjxcljvlkjsgkljsfg\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tlaskjlkjsakljglsdjfgksdjlkgjdlskjb\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tlkajsgfljfklgjlkdjgfklsdjklj\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tlkjfglkjlkgjlkjl.aslkjflasjlajglkjaf\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tlkjasflgjlskjglkfjgklgsdjflkbjsdklfjskldfjgklsfdjgklfdjgl\n\tlskadjlkjsldwwwwwfj\n\t\tlkjflkasjlfjlkjajslfkjlasjkdlfjlaskjalvwwwwwwwwwwwwwwwkjlsjfglkjalsjgflkjaljlkdsjslbjsljksldjlsjdlkjljvblkjlkajfljgasljfkajgfljfjgldjblkjsdljgsldjg.skljf"));
+    }
+
+    /**
+     * 执行用时： 1 ms , 在所有 Java 提交中击败了 65.17% 的用户
+     * 内存消耗： 39 MB , 在所有 Java 提交中击败了 74.54% 的用户
+     * 通过测试用例： 26 / 26
+     *
+     * @param input
+     * @return
+     */
+    public int lengthLongestPath(String input) {
+        int countWord = 0;
+        int countSlashT = 0;
+        LinkedList<Integer> stack = new LinkedList<>();
+        int totalLength = 0;
+        boolean file = false;
+        int longestLen = 0;
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == '\n') {
+                while (countSlashT < stack.size()) {
+                    totalLength -= stack.pop();
+                }
+                stack.push(countWord);
+                totalLength += countWord;
+                if (file && totalLength + stack.size() - 1 > longestLen) {
+                    longestLen = totalLength + stack.size() - 1;
+                }
+                countWord = 0;
+                countSlashT = 0;
+                file = false;
+            } else if (input.charAt(i) == '\t') {
+                countSlashT++;
+            } else if (input.charAt(i) == '.') {
+                countWord++;
+                file = true;
+            } else {
+                countWord++;
+            }
+        }
+        while (countSlashT < stack.size()) {
+            totalLength -= stack.pop();
+        }
+        stack.push(countWord);
+        totalLength += countWord;
+        if (file && totalLength + stack.size() - 1 > longestLen) {
+            longestLen = totalLength + stack.size() - 1;
+        }
+        return longestLen;
+    }
+
+    public static void main(String[] args) {
+        String test = "sladjf\n\tlkjlkv\n\t\tlkjlakjlert\n\t\t\tlaskjglaksjf\n\t\t\t\tlakjgfljrtlj\n\t\t\t\t\tlskajflakjsvlj\n\t\t\t\t\t\tlskgjflkjrtlrjt\n\t\t\t\t\t\t\tlkjglkjlvkjdlvkj\n\t\t\t\t\t\t\t\tlfjkglkjfljdlv\n\t\t\t\t\t\t\t\t\tlkdfjglerjtkrjkljsd.lkvjlkajlfk\n\t\t\t\t\t\t\tlskfjlksjljslvjxjlvkzjljajoiwjejlskjslfj.slkjflskjldfkjoietruioskljfkljf\n\t\t\t\t\tlkasjfljsaljlxkcjzljvl.asljlksaj\n\t\t\t\tasldjflksajf\n\t\t\t\talskjflkasjlvkja\n\t\t\t\twioeuoiwutrljsgfjlskfg\n\t\t\t\taslkjvlksjvlkjsflgj\n\t\t\t\t\tlkvnlksfgk.salfkjaslfjskljfv\n\t\t\tlksdjflsajlkfj\n\t\t\tlasjflaskjlk\n\t\tlsakjflkasjfkljas\n\t\tlskjvljvlkjlsjfkgljfg\n\tsaljkglksajvlkjvkljlkjvksdj\n\tlsakjglksajkvjlkjdklvj\n\tlskjflksjglkdjbkljdbkjslkj\n\t\tlkjglkfjkljsdflj\n\t\t\tlskjfglkjdfgkljsdflj\n\t\t\t\tlkfjglksdjlkjbsdlkjbk\n\t\t\t\t\tlkfgjlejrtljkljsdflgjl\n\t\t\t\t\tsalgkfjlksfjgkljsgfjl\n\t\t\t\t\tsalkflajwoieu\n\t\t\t\t\t\tlaskjfglsjfgljkkvjsdlkjbklds\n\t\t\t\t\t\t\tlasjglriotuojgkjsldfgjsklfgjl\n\t\t\t\t\t\t\t\tlkajglkjskljsdljblkdfjblfjlbjs\n\t\t\t\t\t\t\t\t\tlkajgljroituksfglkjslkjgoi\n\t\t\t\t\t\t\t\t\t\tlkjglkjkljkljdkbljsdfljgklfdj\n\t\t\t\t\t\t\t\t\t\t\tlkjlgkjljgslkdkldjblkj\n\t\t\t\t\t\t\t\t\t\t\t\tlkjfglkjlkjbsdklj.slgfjalksjglkfjglf\n\t\t\t\t\t\t\t\t\t\t\t\tlkasjrlkjwlrjljsl\n\t\t\t\t\t\t\t\t\t\t\t\t\tlksjgflkjfklgjljbljls\n\t\t\t\t\t\t\t\t\t\t\t\t\t\tlkjsglkjlkjfkljdklbjkldf\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tlkjglkjdlsfjdglsdjgjlxljjlrjsgjsjlk\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tlkjsgkllksjfgjljdslfkjlkasjdflkjxcljvlkjsgkljsfg\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tlaskjlkjsakljglsdjfgksdjlkgjdlskjb\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tlkajsgfljfklgjlkdjgfklsdjklj\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tlkjfglkjlkgjlkjl.aslkjflasjlajglkjaf\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tlkjasflgjlskjglkfjgklgsdjflkbjsdklfjskldfjgklsfdjgklfdjgl\n\tlskadjlkjsldwwwwwfj\n\t\tlkjflkasjlfjlkjajslfkjlasjkdlfjlaskjalvwwwwwwwwwwwwwwwkjlsjfglkjalsjgflkjaljlkdsjslbjsljksldjlsjdlkjljvblkjlkajfljgasljfkajgfljfjgldjblkjsdljgsldjg.skljf";
+        String result = lengthLongestPath_bugFinder(test);
+        System.out.println(result);
+        System.out.println(result.length());
+    }
+
+    public static String lengthLongestPath_bugFinder(String input) {
+        StringBuilder sb = new StringBuilder();
+        StringBuilder sbLong = new StringBuilder();
+        int countSlashT = 0;
+        LinkedList<String> stack = new LinkedList<>();
+        int totalLength = 0;
+        boolean file = false;
+        int longestLen = 0;
+        String longest = "";
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == '\n') {
+                while (countSlashT < stack.size()) {
+                    totalLength -= stack.pop().length();
+                }
+                stack.push(sb.toString());
+                totalLength += sb.length();
+                if (file && totalLength + stack.size() - 1 > longestLen) {
+                    longestLen = totalLength + stack.size() - 1;
+                    sbLong.setLength(0);
+                    for (String s : stack) {
+                        if (sbLong.length() > 0) {
+                            sbLong.append('/');
+                        }
+                        sbLong.append(s);
+                    }
+                    longest = sbLong.toString();
+                }
+                sb.setLength(0);
+                countSlashT = 0;
+                file = false;
+            } else if (input.charAt(i) == '\t') {
+                countSlashT++;
+            } else if (input.charAt(i) == '.') {
+                sb.append(input.charAt(i));
+                file = true;
+            } else {
+                sb.append(input.charAt(i));
+            }
+        }
+        while (countSlashT < stack.size()) {
+            totalLength -= stack.pop().length();
+        }
+        stack.push(sb.toString());
+        totalLength += sb.length();
+        if (file && totalLength + stack.size() - 1 > longestLen) {
+            longestLen = totalLength + stack.size() - 1;
+            sbLong.setLength(0);
+            for (String s : stack) {
+                if (sbLong.length() > 0) {
+                    sbLong.append('/');
+                }
+                sbLong.append(s);
+            }
+            longest = sbLong.toString();
+        }
+        return longest;
+    }
+}
