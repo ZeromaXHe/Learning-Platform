92d4f39539b2ab4f823b697970885b1474fc8701
Zhu XH <zhuxiaohe95@163.com>
2022-06-19 16:31:06 +0800

    perf (leetCodeSolver Java): 力扣第 508 题 Stream 解法和去掉多余的哈希表

--- LeetCodeSolver/src/test/java/leetcode/from501to550/Solution508.java
+++ LeetCodeSolver/src/test/java/leetcode/from501to550/Solution508.java
@@ -3,9 +3,12 @@ package leetcode.from501to550;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * @author ZeromaXHe
@@ -39,7 +42,6 @@ public class Solution508 {
         Assert.assertArrayEquals(new int[]{2, -3, 4}, findFrequentTreeSum(root));
     }
 
-    HashMap<TreeNode, Integer> map;
     HashMap<Integer, Integer> countMap;
     List<Integer> list;
     int maxCount;
@@ -59,15 +61,14 @@ public class Solution508 {
      * |     }
      * | }
      * <p>
-     * 执行用时：4 ms, 在所有 Java 提交中击败了 70.84% 的用户
-     * 内存消耗：42.8 MB, 在所有 Java 提交中击败了 5.13% 的用户
+     * 执行用时：2 ms, 在所有 Java 提交中击败了 100.00% 的用户
+     * 内存消耗：42 MB, 在所有 Java 提交中击败了 10.82% 的用户
      * 通过测试用例： 58 / 58
      *
      * @param root
      * @return
      */
     public int[] findFrequentTreeSum(TreeNode root) {
-        map = new HashMap<>();
         countMap = new HashMap<>();
         list = new LinkedList<>();
         maxCount = 0;
@@ -83,19 +84,10 @@ public class Solution508 {
     private int traversal(TreeNode root) {
         int sum = root.val;
         if (root.left != null) {
-            if (map.containsKey(root.left)) {
-                sum += map.get(root.left);
-            } else {
-                int val = traversal(root.left);
-                sum += val;
-            }
+            sum += traversal(root.left);
         }
         if (root.right != null) {
-            if (map.containsKey(root.right)) {
-                sum += map.get(root.right);
-            } else {
-                sum += traversal(root.right);
-            }
+            sum += traversal(root.right);
         }
         int newCount = countMap.getOrDefault(sum, 0) + 1;
         if (newCount > maxCount) {
@@ -106,7 +98,43 @@ public class Solution508 {
             list.add(sum);
         }
         countMap.put(sum, newCount);
-        map.put(root, sum);
+        return sum;
+    }
+
+    /**
+     * 执行用时：8 ms, 在所有 Java 提交中击败了 10.38% 的用户
+     * 内存消耗：41.8 MB, 在所有 Java 提交中击败了 26.89% 的用户
+     * 通过测试用例：58 / 58
+     *
+     * @param root
+     * @return
+     */
+    public int[] findFrequentTreeSum2(TreeNode root) {
+        HashMap<Integer, Integer> sumToCount = new HashMap<>();
+        traversal(root, sumToCount);
+        int max = sumToCount.values().stream()
+                .max(Comparator.comparingInt(Integer::intValue)).orElse(0);
+        List<Integer> resultList = sumToCount.entrySet().stream()
+                .filter(e -> e.getValue() == max)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+        int[] result = new int[resultList.size()];
+        int i = 0;
+        for (int sum : resultList) {
+            result[i++] = sum;
+        }
+        return result;
+    }
+
+    private int traversal(TreeNode root, HashMap<Integer, Integer> sumToCount) {
+        int sum = root.val;
+        if (root.left != null) {
+            sum += traversal(root.left, sumToCount);
+        }
+        if (root.right != null) {
+            sum += traversal(root.right, sumToCount);
+        }
+        sumToCount.put(sum, sumToCount.getOrDefault(sum, 0) + 1);
         return sum;
     }
 
